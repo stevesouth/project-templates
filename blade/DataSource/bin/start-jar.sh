758c82ed33457102699ad33520d9d4f1819050e5
Christoph Sitter <christoph.sitter@caplin.com>
2016-03-16 18:10:29 +0000

    fixed up kit assembling and start-jar. But logs appear on stdout atm

--- blade/DataSource/bin/start-jar.sh
+++ blade/DataSource/bin/start-jar.sh
@@ -10,13 +10,16 @@
 # Returns the process id of the Java process.
 #
 
+BLADENAME=@capConfAdapterName@
+
 if [ "$1" = "CONFREADER" ]; then
    shift
    confreading=1
    jar=`ls "$BINARY_ROOT"/lib/datasource-java*.jar|head -1`
 else
    confreading=0
-   jar="${BINARY_ROOT}/lib*"
+   jar=`ls "$BINARY_ROOT"/lib/$BLADENAME*.jar|head -1`
+   classpath="${BINARY_ROOT}/lib/*"
 
    echo "Classpath: $jar"
 fi
@@ -25,6 +28,6 @@ if [ $confreading = 1 ]; then
    java -jar "$jar" "$@"
    exit $?
 else
-   java -cp "$jar" "$@" > "$LOGDIR"/java$BLADENAME.log 2>&1 &
+   java -cp "$classpath" -jar "$jar" > "$LOGDIR"/java-$BLADENAME.log 2>&1 &
    echo $!
 fi
--- src/main/java/com/caplin/template/AdapterTemplate.java
+++ src/main/java/com/caplin/template/AdapterTemplate.java
@@ -2,7 +2,6 @@ package com.caplin.template;
 
 import com.caplin.datasource.DataSource;
 import com.caplin.datasource.DataSourceFactory;
-import com.caplin.datasource.namespace.PrefixNamespace;
 import com.caplin.template.pricing.PricingTemplateDataProvider;
 
 public class AdapterTemplate {
